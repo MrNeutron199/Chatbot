2b1e72c581862cd36e7e00c55547761cfcbef441
MrNeutron <rspandit468@gmail.com>
2023-11-14 19:30:39 +0530

    Name and Developer Info

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ function generateReply() {
 		timeDiv.classList.add('time');
 		const chatBody = document.getElementById('chatBody');
 		const message = document.getElementById('message').value;
+		const msg = message.replaceAll('Mylo', '').trim();
 		const messageDiv = document.createElement('div');
 		messageDiv.classList.add('user-message');
 		messageDiv.append(message);
 		chatBody.append(messageDiv);
-
 		const replyDiv = document.createElement('div');
 		replyDiv.classList.add('chatbot-message');
 		replyDiv.innerText = '....';
@@ -20,14 +20,42 @@ function generateReply() {
 			method: 'POST',
 			data: {
 				prompt: {
-					messages: [{ content: message }],
+					messages: [{ content: msg }],
 				},
 			},
 		};
+		function reply(text) {
+			setTimeout(() => {
+				replyDiv.innerText = text;
+			}, 2000);
+			chatBody.scrollTop = chatBody.scrollHeight;
+		}
+		if (message.includes('your') || message.includes('ur')) {
+			if (message.includes('name')) {
+				const arr = [
+					'My name is Mylo',
+					'You can call me Mylo!',
+					'I go by Mylo',
+					"I'm Mylo, Nice to meet you!",
+				];
+				const m = Math.floor(Math.random() * (4 + 1));
+				reply(arr[m]);
+				return;
+			}
+
+			if (message.includes('creator') || message.includes('developer')) {
+				reply('I am developed by Tanmay Sharma, a high school student.');
+				return;
+			}
+		}
+		if (message.includes('created') && message.includes('you')) {
+			reply('I am developed by Tanmay Sharma, a high school student.');
+			return;
+		}
 		axios(options).then((res) => {
 			if (!res.data.candidates) {
 				replyDiv.innerText =
-					'Looks like I am unable to generate a reply.\nPlease contact the Developer.';
+					'Looks like you managed to defeat a Robot!.\n(I ran into an error, Sorry!)';
 				return;
 			}
 			const reply = res.data.candidates[0].content;
