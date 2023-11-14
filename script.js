bdcefef8f1e6d451fdf513f3b62e05c533dc74d9
MrNeutron <rspandit468@gmail.com>
2023-11-14 18:33:18 +0530

    Optimized for mobile phone usage

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 function generateReply() {
 	try {
-		document.getElementById('time').classList.remove('removed');
-		document.getElementById('time').classList.add('time');
+		timeDiv.classList.remove('removed');
+		timeDiv.classList.add('time');
 		const chatBody = document.getElementById('chatBody');
-		const message = document.getElementById('txtInput').value;
-		const messageEle = document.createElement('div');
-		const txtNode = document.createTextNode(message);
-		messageEle.classList.add('user-message');
-		chatBody.append(messageEle);
-		messageEle.append(txtNode);
+		const message = document.getElementById('message').value;
+		const messageDiv = document.createElement('div');
+		messageDiv.classList.add('user-message');
+		messageDiv.append(message);
+		chatBody.append(messageDiv);
+
 		const replyDiv = document.createElement('div');
 		replyDiv.classList.add('chatbot-message');
-		replyDiv.innerText = '...';
+		replyDiv.innerText = 'Loading Reply...';
 		chatBody.append(replyDiv);
 		const key = 'AIzaSyCMVLJ_4GQlJZTp2B6nFmbOSsr4DUin-98';
-		if (!key) throw new Error('No Key provided');
 		const link = `https://generativelanguage.googleapis.com/v1beta2/models/chat-bison-001:generateMessage?key=${key}`;
 		const options = {
 			url: link,
@@ -27,18 +26,16 @@ function generateReply() {
 		};
 		axios(options).then((res) => {
 			if (!res.data.candidates) {
-				return `Looks like you defeated a Robot...`;
+				replyDiv.innerText =
+					'Looks like I am unable to generate a reply.\nPlease contact the Developer.';
+				return;
 			}
 			const reply = res.data.candidates[0].content;
-			if (!reply) {
-				return `Something failed, Sorry!`;
-			}
+			if (!reply) return;
 			replyDiv.innerText = reply;
 		});
 		chatBody.scrollTop = chatBody.scrollHeight;
 	} catch (err) {
-		const a = document.createElement('div');
-		a.append(err.toString());
-		document.getElementById('chatBody').insertAdjacentElement('beforebegin', a);
+		console.log(err);
 	}
 }
