9fab9182ee34333827638a411f70f7f2185f260a
MrNeutron <85393726+MrNeutron199@users.noreply.github.com>
2023-11-22 17:24:40 +0000

    Added animation

--- script.js
+++ script.js
@@ -1,66 +1,77 @@
 function generateReply() {
-	try {
-const message = (document.getElementById('message').value).trim()
-		if (!message.toString()) return;	
+	const message = (document.getElementById('message').value).trim()
+	if (!message.toString()) return;
 	timeDiv.classList.remove('removed');
-		timeDiv.classList.add('time');
-		const chatBody = document.getElementById('chatBody');
-		const msg = message.replaceAll('Mylo', '').toLowerCase();
-		if (!msg.toString()) return;
-		const messageDiv = document.createElement('div');
-		messageDiv.innerText = message;
-		messageDiv.classList.add('user-message');
-		chatBody.append(messageDiv);
-		const replyDiv = document.createElement('div');
-		replyDiv.classList.add('chatbot-message');
-		document.getElementById("loading").classList.remove("removed")
-		const key = 'AIzaSyCMVLJ_4GQlJZTp2B6nFmbOSsr4DUin-98';
-		const link = `https://generativelanguage.googleapis.com/v1beta2/models/chat-bison-001:generateMessage?key=${key}`;
-		const options = {
-			url: link,
-			method: 'POST',
-			data: {
-				prompt: {
-					messages: [{ content: msg }],
-				},
-			},
-		};
-		function reply(text) {
-			setTimeout(() => {
-				replyDiv.append(document.createTextNode(text))
-				document.getElementById("loading").classList.add("removed")
-				chatBody.append(replyDiv)
-			}, 2000);
-			chatBody.scrollTop = chatBody.scrollHeight;
-		}
-		if (msg.includes('your') || msg.includes('ur')) {
-			if (msg.includes('name')) {
-				reply('My name is Mylo. Nice to meet you!');
-				return;
-			}
+	timeDiv.classList.add('time');
+	const chatBody = document.getElementById('chatBody');
+	const msg = message.replaceAll('Mylo', '').toLowerCase();
+	if (!msg.toString()) return;
+	const messageDiv = document.createElement('div');
+	messageDiv.innerText = message;
+	messageDiv.classList.add('user-message');
+	chatBody.append(messageDiv);
+	const replyDiv = document.createElement('div');
+	replyDiv.classList.add('chatbot-message');
 
-			if (message.includes('creator') || message.includes('developer')) {
-				reply('I am developed by Tanmay Sharma, a high school student.');
-				return;
-			}
+	//Animation Area
+	const loadingDiv = document.createElement('div')
+	loadingDiv.classList.add('typing')
+	const span1 = document.createElement('span')
+	const span2 = document.createElement('span')
+	const span3 = document.createElement('span')
+	loadingDiv.append(span1, span2, span3)
+	replyDiv.append(loadingDiv)
+	//Animation Area Finish
+	chatBody.append(replyDiv)
+	const key = 'AIzaSyCMVLJ_4GQlJZTp2B6nFmbOSsr4DUin-98';
+	const link = `https://generativelanguage.googleapis.com/v1beta2/models/chat-bison-001:generateMessage?key=${key}`;
+	const options = {
+		url: link,
+		method: 'POST',
+		data: {
+			prompt: {
+				messages: [{ content: msg }],
+			},
+		},
+	};
+	function reply(text) {
+		setTimeout(() => {
+			document.getElementsByClassName('typing')
+			replyDiv.replaceChildren(document.createTextNode(text))
+			chatBody.append(replyDiv)
+			chatBody.scrollTop = chatBody.scrollHeight;
+		}, 2000);
+	}
+
+	//Hard Coded checks
+	if (msg.includes('your') || msg.includes('ur')) {
+		if (msg.includes('name')) {
+			reply('My name is Mylo. Nice to meet you!');
+			return;
 		}
-		if (message.includes('created') && message.includes('you')) {
+
+		if (message.includes('creator') || message.includes('developer')) {
 			reply('I am developed by Tanmay Sharma, a high school student.');
 			return;
 		}
-		axios(options).then((res) => {
-			if (!res.data.candidates) {
-				reply(`I'm sorry, but I'm not sure how to answer that. Could you try rephrasing your question or providing more detail?`)
-				return;
-			}
-			const repl = res.data.candidates[0].content;
-			if (!repl) return;
-			reply(repl)
-		});
-	} catch (err) {
-		console.log(err)
-		reply(`I'm sorry, but I'm not sure if I can answer that.`);
-		document.getElementById("loading").classList.add("removed")
+		if (message.includes('age')) {
+			reply("I was born a week ago. Ill assume I'm a week old.")
+			return;
+		}
+	}
+	if (message.includes('created') && message.includes('you')) {
+		reply('I am developed by Tanmay Sharma, a high school student.');
 		return;
 	}
+	//Hard coded checks finished
+
+	axios(options).then((res) => {
+		if (!res.data.candidates) {
+			reply(`I'm sorry, but I'm not sure how to answer that. Could you try rephrasing your question or providing more detail?`)
+			return;
+		}
+		const repl = res.data.candidates[0].content;
+		if (!repl) return;
+		reply(repl)
+	});
 }
